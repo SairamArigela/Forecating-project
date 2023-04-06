69c72cd75df88421dd19cb4e4e004b526c64f3e1
SairamArigela <130032664+SairamArigela@users.noreply.github.com>
2023-04-06 11:48:59 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 05 18:06:17 2023
+
+"""
+
+
+
+import pandas as pd
+import streamlit as st 
+from statsmodels.tsa.holtwinters import ExponentialSmoothing 
+from pickle import dump
+from pickle import load
+
+st.title('Model Deployment: Forecasting')
+
+st.sidebar.header('User Input Parameters')
+
+    
+Days = st.sidebar.number_input("number of days for forecasting",min_value=1, step=1)
+st.subheader('User Input parameters')
+st.write(Days)
+
+
+
+# load the model from disk
+loaded_model = load(open('C:\\Users\\Siva\\Desktop\\ExcelR\\Project\\final_model.sav', 'rb'))
+
+prediction = loaded_model.forecast(Days)
+
+
+
+
+st.subheader('Predicted Values')
+st.write(prediction)
+
+
