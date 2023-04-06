3b575e0aca3ad3d847b59130a63508eb8391b72b
SairamArigela <130032664+SairamArigela@users.noreply.github.com>
2023-04-06 22:47:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ import streamlit as st
 
 from pickle import dump
 from pickle import load
+import pickle
 import os
 from pathlib import Path
 
@@ -28,7 +29,7 @@ st.write(Days)
 # load the model from disk
 current_directory = Path(__file__).parent
 open_model=open(os.path.join(current_directory,'final_model.sav'), 'rb')
-loaded_model = load(open_model)
+loaded_model = pickle.load(open_model)
 
 prediction = loaded_model.forecast(Days)
 
